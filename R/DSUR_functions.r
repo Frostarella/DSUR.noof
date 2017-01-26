3002fdc6fe8f55d859fa4454cba8af0db495d48b
Maria Aufheimer <maria.aufheimer@uni-ulm.de>
2017-01-26 18:28:09 +0100

    general modification and update; check() only produces two NOTEs now; some examples missing

--- R/DSUR_functions.r
+++ R/DSUR_functions.r
@@ -5,6 +5,7 @@
 #'
 #' @param t t-value of the test.
 #' @param df degrees of freedom.
+#' @return None
 #' @export
 #' @examples 
 #' rcontrast(2.474, 12)
@@ -20,13 +21,14 @@ rcontrast <- function(t, df) {
 #'
 #' @param wilcoxModel a model created using wilcox.test().
 #' @param N total sample size.
+#' @return ans list of calculation results
 #' @export
-#' @examples
-#' rFromWilcox(sunModel, 20)
+# @examples
+# rFromWilcox(sunModel, 20)
 rFromWilcox <- function(wilcoxModel, N) {
   z <- qnorm(wilcoxModel$p.value/2)
   r <- z/sqrt(N)
-  cat(WilcoxModel$data.name, "Effect Size, r = ", r)
+  cat(wilcoxModel$data.name, "Effect Size, r = ", r)
 }
 
 #' KMO Kaiser-Meyer-Olkin Measure of Sampling Adequacy 
@@ -38,54 +40,58 @@ rFromWilcox <- function(wilcoxModel, N) {
 #' Needs the MASS package.
 #'
 #' @param data dataframe to compute the kmo on.
+#' @return None
 #' @export
 #' @examples
-#' kmo(raqData)
+#' kmo(data.frame(c(1,2,3), c(1,2,3), c(1,2,3)))
 kmo <- function(data){
+  if(requireNamespace("MASS", quietly = TRUE)) {
+    
+    X <- cor(as.matrix(data)) 
+    iX <- MASS::ginv(X) 
+    S2 <- diag(diag((iX^-1)))
 
-  library(MASS) 
-  X <- cor(as.matrix(data)) 
-  iX <- ginv(X) 
-  S2 <- diag(diag((iX^-1)))
-
-  AIS <- S2%*%iX%*%S2                      # anti-image covariance matrix
-  IS <- X+AIS-2*S2                         # image covariance matrix
-  Dai <- sqrt(diag(diag(AIS)))
-  IR <- ginv(Dai)%*%IS%*%ginv(Dai)         # image correlation matrix
-  AIR <- ginv(Dai)%*%AIS%*%ginv(Dai)       # anti-image correlation matrix
-  a <- apply((AIR - diag(diag(AIR)))^2, 2, sum)   
-  AA <- sum(a) 
-  b <- apply((X - diag(nrow(X)))^2, 2, sum)   
-  BB <- sum(b)
-  MSA <- b/(b+a)                        # indiv. measures of sampling adequacy
-  AIR <- AIR-diag(nrow(AIR))+diag(MSA)  # Examine the anti-image of the
+    AIS <- S2%*%iX%*%S2                      # anti-image covariance matrix
+    IS <- X+AIS-2*S2                         # image covariance matrix
+    Dai <- sqrt(diag(diag(AIS)))
+    IR <- MASS::ginv(Dai)%*%IS%*%MASS::ginv(Dai)         # image correlation matrix
+    AIR <- MASS::ginv(Dai)%*%AIS%*%MASS::ginv(Dai)       # anti-image correlation matrix
+    a <- apply((AIR - diag(diag(AIR)))^2, 2, sum)   
+    AA <- sum(a) 
+    b <- apply((X - diag(nrow(X)))^2, 2, sum)   
+    BB <- sum(b)
+    MSA <- b/(b+a)                        # indiv. measures of sampling adequacy
+    AIR <- AIR-diag(nrow(AIR))+diag(MSA)  # Examine the anti-image of the
                                         # correlation matrix. That is the
                                         # negative of the partial correlations,
                                         # partialling out all other variables.
-  kmo <- BB/(AA+BB)                     # overall KMO statistic
+    kmo <- BB/(AA+BB)                     # overall KMO statistic
 
-  # Reporting the conclusion 
-  if (kmo >= 0.00 && kmo < 0.50){ 
-    test <- 'The KMO test yields a degree of common variance unacceptable for FA.'
-  } else if (kmo >= 0.50 && kmo < 0.60){ 
-    test <- 'The KMO test yields a degree of common variance miserable.' 
-  } else if (kmo >= 0.60 && kmo < 0.70){ 
-    test <- 'The KMO test yields a degree of common variance mediocre.' 
-  } else if (kmo >= 0.70 && kmo < 0.80){ 
-    test <- 'The KMO test yields a degree of common variance middling.' 
-  } else if (kmo >= 0.80 && kmo < 0.90){ 
-    test <- 'The KMO test yields a degree of common variance meritorious.' 
-  } else { 
-    test <- 'The KMO test yields a degree of common variance marvelous.'     
-  }
+    # Reporting the conclusion 
+    if (kmo >= 0.00 && kmo < 0.50){ 
+      test <- 'The KMO test yields a degree of common variance unacceptable for FA.'
+    } else if (kmo >= 0.50 && kmo < 0.60){ 
+      test <- 'The KMO test yields a degree of common variance miserable.' 
+    } else if (kmo >= 0.60 && kmo < 0.70){ 
+      test <- 'The KMO test yields a degree of common variance mediocre.' 
+    } else if (kmo >= 0.70 && kmo < 0.80){ 
+      test <- 'The KMO test yields a degree of common variance middling.' 
+    } else if (kmo >= 0.80 && kmo < 0.90){ 
+      test <- 'The KMO test yields a degree of common variance meritorious.' 
+    } else { 
+      test <- 'The KMO test yields a degree of common variance marvelous.'     
+    }
 
-  ans <- list( overall = kmo,
+    ans <- list( overall = kmo,
 		report = test,
 		individual = MSA,
 		AIS = AIS,
                 AIR = AIR )
 
-  return(ans)
+    return(ans)
+  } else {
+    stop("Package 'MASS' (version >= 7.3-45) needed. Please install.", call. = FALSE)
+  }
 } # end of kmo()
 
 # Function factor.structure
@@ -97,9 +103,10 @@ kmo <- function(data){
 #' This function calculates the mean value of a variable.
 #'
 #' @param variable the variable to calculate the mean value of.
+#' @return None
 #' @export
 #' @examples 
-#' meanOfVariable(lecturerFriends)
+#' meanOfVariable(c(1,2,3))
 meanOfVariable <- function(variable) {
   mean <- sum(variable)/length(variable) 
   cat("Mean = ", mean)
@@ -111,9 +118,10 @@ meanOfVariable <- function(variable) {
 #' This function calculates the various values of R^2 from a logistic regression model.
 #'
 #' @param logModel logistic regression model
+#' @return None
 #' @export
-#' @examples
-#' logisticPseudoR2s(eelModel.1)
+# @examples
+# logisticPseudoR2s(eelModel.1)
 logisiticPseudoR2s <- function(logModel) {
   dev <- logModel$deviance
   nullDev <- logModel$null.deviance
@@ -133,20 +141,21 @@ logisiticPseudoR2s <- function(logModel) {
 #' DSUR, page 365.
 #' This function adjusts scores for the fact they were from a repeated-measures design.
 #'
-#' @param dataframe dataframe with the scores that need adjustment.
+#' @param dataframe 2-dimensional dataframe with the scores that need adjustment.
+#' @return output table of variables and adjustment.
 #' @export
 #' @examples
-#' rmMeanAdjust(spiderWide)
+#' rmMeanAdjust(data.frame(c(1,2,3), c(1,2,3)))
 rmMeanAdjust <- function(dataframe) {
   varNames <- names(dataframe)
   pMean <- (dataframe[,1] + dataframe[,2])/2
-  grandmean <- mean(c(dataframe[,1], dataframe[,2]))
-  adj <- adj <- gradmean -pMean
+  gradmean <- mean(c(dataframe[,1], dataframe[,2]))
+  adj <- adj <- gradmean - pMean
   varA_adj <- dataframe[,1] + adj 
   varB_adj <- dataframe[,2] + adj 
   output <- data.frame(varA_adj, varB_adj)
-  names(output) <- c(paste(varNames[1], "Adj", sep="_",
-			    varNames[2], "Adj", sep="_"))
+  names(output) <- c(paste(varNames[1], "Adj", sep="_"), 
+		      paste(varNames[2], "Adj", sep="_"))
   return(output)
 }
 
@@ -161,9 +170,10 @@ rmMeanAdjust <- function(dataframe) {
 #' @param sd2 standard deviation of group 2.
 #' @param n1 sample size of group 1.
 #' @param n2 sample size of group 2.
+#' @return None
 #' @export
-#' @examples
-#' ttestfromMeans(x1, x2, sd1, sd2, n1, n2)
+# @examples
+# ttestfromMeans(x1, x2, sd1, sd2, n1, n2)
 ttestfromMeans <- function(x1, x2, sd1, sd2, n1, n2) {
   df <- n1+n2-2
   poolvar <- (((n1-1)*sd1^2)+((n2-1)*sd2^2))/df
@@ -178,8 +188,10 @@ ttestfromMeans <- function(x1, x2, sd1, sd2, n1, n2) {
 #' This function wraps all of the factor analysis residual commands.
 #'
 #' @param matrix residual matrix. (Calculate using factor.residuals()).
+#' @return None
 #' @export
-#' @examples residual.stats(resids)
+# @examples 
+# residual.stats(resids)
 residual.stats <- function(matrix) {
   residuals <- as.matrix(matrix[upper.tri(matrix)]) 
   large.resid <- abs(residuals) > 0.05
