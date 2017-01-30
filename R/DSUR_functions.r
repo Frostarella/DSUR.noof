2667ae83d72723b365cadc0ef5c1eb73c872e092
Maria Aufheimer <maria.aufheimer@uni-ulm.de>
2017-01-30 19:43:51 +0100

    updated documentation, DESCRIPTION, and README

--- R/DSUR_functions.r
+++ R/DSUR_functions.r
@@ -1,71 +1,68 @@
 #' Function rcontrast
 #'
-#' DSUR, page 457.
-#' This function computes effect sizes for the orthogonal contrasts.
+#' This function computes effect sizes for the orthogonal contrasts and prints the result.
 #'
 #' @param t t-value of the test.
 #' @param df degrees of freedom.
 #' @return None
 #' @export
+#' @source Andy Field, 'Discovering Statistics Using R', p. 457
 #' @examples 
 #' rcontrast(2.474, 12)
 rcontrast <- function(t, df) {
-  r <- sqrt(t^2/(t^2 + df)) 
+  r <- sqrt(t^2 / (t^2 + df)) 
   print(paste("r = ", r))
 }
   
 #' Function rFromWilcox 
 #'
 #' DSUR, page 665.
-#' This function computes effect sizes from models created using wilcox.test().
+#' This function computes effect sizes from models created using wilcox.test() and prints the result.
 #'
 #' @param wilcoxModel a model created using wilcox.test().
 #' @param N total sample size.
-#' @return ans list of calculation results
 #' @export
-# @examples
-# rFromWilcox(sunModel, 20)
+#' @source Andy Field, 'Discovering Statistics Using R', p. 665
+# @examples missing
 rFromWilcox <- function(wilcoxModel, N) {
-  z <- qnorm(wilcoxModel$p.value/2)
-  r <- z/sqrt(N)
+  z <- stats::qnorm(wilcoxModel$p.value / 2)
+  r <- z / sqrt(N)
   cat(wilcoxModel$data.name, "Effect Size, r = ", r)
 }
 
 #' KMO Kaiser-Meyer-Olkin Measure of Sampling Adequacy 
 #'
-#' DSUR, page 776.
-#' Function by G. Jay Kerns, Youngstown State University
-#' see http://tolstoy.newcastle.edu.au/R/e2/help/07/08/22816/html
+#' Function based on an implementation by G. Jay Kerns.
 #' This function computes Kaiser-Meyer-Olkin Measure of Sampling Adequacy.
 #' Needs the MASS package.
 #'
 #' @param data dataframe to compute the kmo on.
-#' @return None
+#' @return list of the calculated results.
 #' @export
-#' @examples
-#' kmo(data.frame(c(1,2,3), c(1,2,3), c(1,2,3)))
+#' @source Andy Field, 'Discovering Statistics Using R', p. 776. G. Jay Kerns, http://tolstoy.newcastle.edu.au/R/e2/help/07/08/22816.html
+# @examples missing
 kmo <- function(data){
   if(requireNamespace("MASS", quietly = TRUE)) {
     
-    X <- cor(as.matrix(data)) 
-    iX <- MASS::ginv(X) 
-    S2 <- diag(diag((iX^-1)))
+    x <- stats::cor(as.matrix(data)) 
+    ix <- MASS::ginv(x) 
+    S2 <- diag(diag((ix ^ -1)))
 
-    AIS <- S2%*%iX%*%S2                      # anti-image covariance matrix
-    IS <- X+AIS-2*S2                         # image covariance matrix
+    AIS <- S2 %*% ix %*% S2				# anti-image covariance matrix
+    IS <- x+AIS-2*S2					# image covariance matrix
     Dai <- sqrt(diag(diag(AIS)))
-    IR <- MASS::ginv(Dai)%*%IS%*%MASS::ginv(Dai)         # image correlation matrix
-    AIR <- MASS::ginv(Dai)%*%AIS%*%MASS::ginv(Dai)       # anti-image correlation matrix
-    a <- apply((AIR - diag(diag(AIR)))^2, 2, sum)   
+    IR <- MASS::ginv(Dai) %*% IS %*% MASS::ginv(Dai)	# image correlation matrix
+    AIR <- MASS::ginv(Dai) %*% AIS %*% MASS::ginv(Dai)	# anti-image correlation matrix
+    a <- apply((AIR - diag(diag(AIR))) ^ 2, 2, sum)   
     AA <- sum(a) 
-    b <- apply((X - diag(nrow(X)))^2, 2, sum)   
+    b <- apply((x - diag(nrow(x))) ^ 2, 2, sum)   
     BB <- sum(b)
-    MSA <- b/(b+a)                        # indiv. measures of sampling adequacy
-    AIR <- AIR-diag(nrow(AIR))+diag(MSA)  # Examine the anti-image of the
-                                        # correlation matrix. That is the
-                                        # negative of the partial correlations,
-                                        # partialling out all other variables.
-    kmo <- BB/(AA+BB)                     # overall KMO statistic
+    MSA <- b / (b + a)					# indiv. measures of sampling adequacy
+    AIR <- AIR-diag(nrow(AIR)) + diag(MSA)		# Examine the anti-image of the
+							# correlation matrix. That is the
+							# negative of the partial correlations,
+							# partialling out all other variables.
+    kmo <- BB/(AA+BB)					# overall KMO statistic
 
     # Reporting the conclusion 
     if (kmo >= 0.00 && kmo < 0.50){ 
@@ -82,11 +79,11 @@ kmo <- function(data){
       test <- 'The KMO test yields a degree of common variance marvelous.'     
     }
 
-    ans <- list( overall = kmo,
+    ans <- list(overall = kmo,
 		report = test,
 		individual = MSA,
 		AIS = AIS,
-                AIR = AIR )
+                AIR = AIR)
 
     return(ans)
   } else {
@@ -99,36 +96,35 @@ kmo <- function(data){
 
 #' Function meanOfVariable
 #'
-#' DSUR, page 228.
-#' This function calculates the mean value of a variable.
+#' This function calculates the mean value of a variable and prints the result
 #'
 #' @param variable the variable to calculate the mean value of.
 #' @return None
 #' @export
+#' @source Andy Field, 'Discovering Statistics Using R', p. 228
 #' @examples 
 #' meanOfVariable(c(1,2,3))
 meanOfVariable <- function(variable) {
-  mean <- sum(variable)/length(variable) 
+  mean <- sum(variable) / length(variable) 
   cat("Mean = ", mean)
 }
 
 #' Function logisticsPseudoR2s
-#' 
-#' DSUR, page 332.
-#' This function calculates the various values of R^2 from a logistic regression model.
+#'
+#' This function calculates the various values of R^2 from a logistic regression model and prints the results.
 #'
 #' @param logModel logistic regression model
 #' @return None
 #' @export
-# @examples
-# logisticPseudoR2s(eelModel.1)
+#' @source Andy Field, 'Discovering Statistics Using R', p. 332
+# @examples missing
 logisiticPseudoR2s <- function(logModel) {
   dev <- logModel$deviance
   nullDev <- logModel$null.deviance
   modelN <- length(logModel$fitted.values)
-  R.l <- 1-dev/nullDev
-  R.cs <- 1-exp(-(nullDev-dev)/modelN)
-  R.n <- R.cs/(1-(exp(-(nullDev/modelN))))
+  R.l <- 1 - dev / nullDev
+  R.cs <- 1 - exp( -(nullDev - dev) / modelN)
+  R.n <- R.cs / (1 - (exp( -(nullDev / modelN))))
   
   cat("Pseudo R^2 for logistics regression\n")
   cat("Hosmer and Lemeshow R^2: \t", round(R.l, 3), "\n")
@@ -138,21 +134,20 @@ logisiticPseudoR2s <- function(logModel) {
 
 #' Function rmMeanAdjust
 #'
-#' DSUR, page 365.
 #' This function adjusts scores for the fact they were from a repeated-measures design.
 #'
 #' @param dataframe 2-dimensional dataframe with the scores that need adjustment.
 #' @return output table of variables and adjustment.
 #' @export
-#' @examples
-#' rmMeanAdjust(data.frame(c(1,2,3), c(1,2,3)))
+#' @source Andy Field, 'Discovering Statistics Using R', p. 365
+# @examples missing
 rmMeanAdjust <- function(dataframe) {
   varNames <- names(dataframe)
-  pMean <- (dataframe[,1] + dataframe[,2])/2
-  gradmean <- mean(c(dataframe[,1], dataframe[,2]))
+  pMean <- (dataframe[, 1] + dataframe[, 2]) / 2
+  gradmean <- mean(c(dataframe[, 1], dataframe[, 2]))
   adj <- adj <- gradmean - pMean
-  varA_adj <- dataframe[,1] + adj 
-  varB_adj <- dataframe[,2] + adj 
+  varA_adj <- dataframe[, 1] + adj 
+  varB_adj <- dataframe[, 2] + adj 
   output <- data.frame(varA_adj, varB_adj)
   names(output) <- c(paste(varNames[1], "Adj", sep="_"), 
 		      paste(varNames[2], "Adj", sep="_"))
@@ -161,8 +156,7 @@ rmMeanAdjust <- function(dataframe) {
 
 #' Function ttestfromMeans
 #'
-#' DSUR, page 375.
-#' This function calculates a t-test for the means, standard deviations, and sample sizes of two groups.
+#' This function calculates a t-test for the means, standard deviations, and sample sizes of two groups, and prints the results.
 #'
 #' @param x1 mean of group 1.
 #' @param x2 mean of group 2.
@@ -172,37 +166,36 @@ rmMeanAdjust <- function(dataframe) {
 #' @param n2 sample size of group 2.
 #' @return None
 #' @export
-# @examples
-# ttestfromMeans(x1, x2, sd1, sd2, n1, n2)
+#' @source Andy Field, 'Discovering Statistics Using R', p. 375
+# @examples missing
 ttestfromMeans <- function(x1, x2, sd1, sd2, n1, n2) {
   df <- n1+n2-2
-  poolvar <- (((n1-1)*sd1^2)+((n2-1)*sd2^2))/df
-  t <- (x1-x2)/sqrt(poolvar*((1/n1)+(1/n2)))
-  sig <- 2*(1-pt(abs(t),df))
+  poolvar <- (((n1 - 1) * sd1 ^ 2) + ((n2 - 1) * sd2 ^ 2)) / df
+  t <- (x1 - x2) / sqrt(poolvar * ((1 / n1) + (1 / n2)))
+  sig <- 2 * (1 - stats::pt(abs(t), df))
   paste("t(df = ", df, ") = ", t, ", p = ", sig, sep= "")
 }
 
 #' Function residual.stats
 #'
-#' DSUR, page 785.
-#' This function wraps all of the factor analysis residual commands.
+#' This function wraps all of the factor analysis residual commands and prints the results.
 #'
-#' @param matrix residual matrix. (Calculate using factor.residuals()).
+#' @param matrix residual matrix. (Calculate matrix using factor.residuals()).
 #' @return None
 #' @export
-# @examples 
-# residual.stats(resids)
+#' @source Andy Field, 'Discovering Statistics Using R', p. 785
+# @examples missing
 residual.stats <- function(matrix) {
   residuals <- as.matrix(matrix[upper.tri(matrix)]) 
   large.resid <- abs(residuals) > 0.05
   numberLargeResids <-(large.resid)
   propLargeResid <- numberLargeResids/nrow(residuals) 
-  rmsr <- sqrt(mean(residuals^2))
+  rmsr <- sqrt(mean(residuals ^ 2))
   
   cat("Root means squared residual = ", rmsr, "\n")
   cat("Number of absolute residuals > 0.05 = ", numberLargeResids, "\n")
   cat("Proportions of absolute residuals > 0.05 = ", propLargeResid, "\n")
-  hist(residuals)
+  graphics::hist(residuals)
 }
 
 
--- README.md
+++ README.md
@@ -1,10 +1,23 @@
 # DSUR.noof
-DSUR.noof is a package for R, including (almost) all functions of the DSUR package mentioned in Andy Field's "Discovering Statistics Using R". This is by no means an official version of the DSUR package -- just my private collection which I wanted to share.
 
-# Installation
-The package can be easily installed by using the devtools (install.package("devtools")).
-Simply type the command install_github('DSUR.noof', 'Frostarella') into your R console.
+## Description
+DSUR.noof is a package for R, including (almost) all functions of the DSUR package mentioned in Andy Field's 'Discovering Statistics Using R'. This is by no means an official version of the DSUR package -- just my private collection which I wanted to share.
 
-# Credits
-* Credits for the functions in the package go to Andy Field ("Discovering Statistics Using R") and G. Jay Kerns, Youngstown State University.
-* This package was made by following Hilary Parker's blog post on creating R packages ([read it here!] (https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/))
+## Development
+This package is still a work in progress. It should be working, but the functions are not fully documented yet. Please refer to the book 'Discovering Statistics Using R' when in need of further explanation for a function.
+
+## Installation
+The package can be easily installed by using the devtools to install it directly from GitHub. 
+Just copy the following commands into your R console and execute them.
+```R
+package_install("devtools")
+library(devtools)
+install_github("Frostarella/DSUR.noof")
+```
+
+## Credits
+* Credit goes to Andy Field, Jeremy Miles, and Zoë Field (Authors of 'Discovering Statistics Using R'), as well as G. Jay Kerns, for the functions in this package.
+* This package was made by following several online resources on how to write R packages:
+    * Hilary Parker's blog post ([read it here](https://hilaryparker.com/2014/04/29/writing-an-r-package-from-scratch/))
+    * Karl Broman's tutorial ([read it here](http://kbroman.org/pkg_primer/))
+    * Hadley Wickham's book (only parts of it, though) ([read it here](http://r-pkgs.had.co.nz))
