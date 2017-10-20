9f9dc1f79135924dd5c7c182fe97044916656fa1
Maria Aufheimer <maria.aufheimer@uni-ulm.de>
2017-10-20 11:15:04 +0200

    added data file conversion script and converted example data

---
+++ R/data.R
@@ -0,0 +1,15 @@
+#' Title of Dataset
+#' 
+#' short description of dataset
+#' can be multiple lines long
+#'
+#' @format A data frame with x rows and y variables:
+#' \describe{
+#'  \item{variable1}{variable1 is the first variable, describe unit}
+#'  \item{variable2}{varible2 is the second variabble, describe unit}
+#'  ...
+#' }
+#' @source \url{www.example.com}
+"filename"
+
+
